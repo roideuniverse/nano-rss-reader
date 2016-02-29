ab302da6a0940b5e35e560ac899de8f3a4c4119c
Kaushik Saurabh <roideuniverse@gmail.com>
2016-02-28 23:11:56 -0500

    feat:Remove redundant dependencies

--- XYZReader/src/main/java/com/example/xyzreader/ui/activity/ArticleDetailActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/activity/ArticleDetailActivity.java
@@ -1,16 +1,15 @@
 package com.example.xyzreader.ui.activity;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.LoaderManager;
-import android.content.Loader;
 import android.database.Cursor;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v13.app.FragmentStatePagerAdapter;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBarActivity;
 import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +18,13 @@ import android.view.WindowInsets;
 import com.example.xyzreader.R;
 import com.example.xyzreader.db.ArticleCursorLoader;
 import com.example.xyzreader.db.ItemsContract;
+import com.example.xyzreader.ui.base.BaseActivity;
 import com.example.xyzreader.ui.fragment.ArticleDetailFragment;
 
 /**
  * An activity representing a single Article detail screen, letting you swipe between articles.
  */
-public class ArticleDetailActivity extends ActionBarActivity
+public class ArticleDetailActivity extends BaseActivity
         implements LoaderManager.LoaderCallbacks<Cursor>
 {
 
@@ -52,9 +52,9 @@ public class ArticleDetailActivity extends ActionBarActivity
         }
         setContentView(R.layout.activity_article_detail);
 
-        getLoaderManager().initLoader(0, null, this);
+        getSupportLoaderManager().initLoader(0, null, this);
 
-        mPagerAdapter = new MyPagerAdapter(getFragmentManager());
+        mPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
         mPager = (ViewPager) findViewById(R.id.pager);
         mPager.setAdapter(mPagerAdapter);
         mPager.setPageMargin((int) TypedValue
---
+++ XYZReader/src/main/java/com/example/xyzreader/ui/base/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.example.xyzreader.ui.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by roide on 2/28/16.
+ */
+public abstract class BaseActivity extends AppCompatActivity
+{
+}
